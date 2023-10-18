85defec43513b62e0dc985471d243b3cab3c6d15
juliettec-19 <148246245+juliettec-19@users.noreply.github.com>
2023-10-17 20:56:38 -0400

    Update début fichier projet.py

--- début fichier projet.py
+++ début fichier projet.py
@@ -5,7 +5,6 @@ Created on Sun Oct  8 20:11:38 2023
 @author: noahd
 """
 
-
 import numpy as np
 from MEC1315_STL import *
 from mes_fonctions_complets import *
@@ -24,18 +23,56 @@ Anneau_s1=rep_circulaire(mini_planete, 120, [120, 0, 10], 1/50)
 Anneau_s2=rep_circulaire(mini_planete, 120, [120, 0, -10], 1/50)
 
 
+#Drapeau
+    #dimensions
+cylindre = affinite_vectorielle(cylindre, 0.5, 0.5, 3)
+triangle = affinite_vectorielle(triangle, 1, 6, 4)
+triangle = homothetie(triangle, 0.15)
+    
+    #Placer le triangle en haut et à gauche du cylindre
+deplacement_en_y = max(cylindre[1][:,1])
+deplacement_en_z = max(cylindre[1][:,2])-(max(triangle[1][:,2])-min(triangle[1][:,2]))
+deplacement_triangle = np.array([0, deplacement_en_y, deplacement_en_z])
+triangle = translation(triangle, deplacement_triangle)
+    
+drapeau = fusion((cylindre, triangle))
+drapeau = homothetie(drapeau, 12)
+
+
+#Minion drapeau
+drapeau = centrer(drapeau)
+drapeau = translation(drapeau, np.array([35, -2, -1])) # translation de 2 unités vers les y négatifs et 1 unitées vers les z négatifs
+minion = centrer(homothetie(minion, 1.25))
+minion_drapeau= fusion((drapeau, minion))
+
 
 #Planète colonisée
-drapeau=fonction_drapeau(cylindre, triangle, 1)
-drapeau_minion=minion_drapeau(drapeau, minion,20)
-planete_col=planete_colonisee(drapeau_minion, planete)
+planete = translation(planete, np.array([0, 0, -(max(planete[1][:,2])-min(planete[1][:,2]))/2])) # translation vers z négatifs de la longueur du rayon
+planete_colonisee = fusion((minion_drapeau, planete)) # fusion des deux objets et création du fichier "planete_colonisee.stl"
+
+
 #Satellite
-satellite=fonction_satellite(cube, -300, 300,10) #Répétiton linéaire dans la fonction
-satellite_rot=rotation(satellite,np.pi/4,[1,0,0])
+rectangles = repetition_rectiligne(affinite_vectorielle(cube, 0.5, 1.5, 0.2), 4, 1)
+
+aile_sup = centrer(rectangles) # les 4 panneaux supérieurs de l'aile
+aile_sup= translation(aile_sup, np.array([0, 9, 0]))
+aile_mid = centrer(affinite_vectorielle(cube, 5, 0.4, 0.5)) # centre de l'aile
+aile_mid = translation(aile_mid,np.array([0.5, 0, -0.2]))
+aile_inf = translation(centrer(rectangles), np.array([0, -0.9, 0])) # les 4 panneaux inférieurs de l'aile
+
+aile_1 = fusion((aile_sup, aile_mid, aile_inf)) # une des ailes du satellite
+aile_2 = translation(rotation(aile_1, np.pi, [0, 0, 1]),np.array([7, 0, 0]))  # la deuxième aile du satellite
+centre = centrer(affinite_vectorielle(cube, 2, 2.5, 2)) # Centre satellite
+centre = translation(centre, np.array([3.5, 0, -0.8]))
+
+satellite = homothetie(fusion((aile_1, aile_2, centre)), 10)
+satellite = rotation(satellite,np.pi/4,[1,0,0])
+satellite = translation(satellite, np.array([-300, 300, 0]))
+
 #Galaxy 
-galaxy=rep_perso(6, planete_col, planete, 2/3) #Répétition selon la suite de fibonacci
+galaxy=rep_perso(6, planete_colonisee, planete, 2/3) #Répétition selon la suite de fibonacci
 
 #fusion et export du fichier STL
-scene=fusion((Anneau_p,Anneau_s1,Anneau_s2,galaxy,satellite_rot))
+scene=fusion((Anneau_p,Anneau_s1,Anneau_s2,galaxy,satellite))
 f,v,n=scene[0],scene[1],scene[2]
 EcrireSTLASCII('scene_final_XX.stl', f, v, n)

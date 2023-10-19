fe6f64398985dba0b29774fdc5fb53400800a569
juliettec-19 <148246245+juliettec-19@users.noreply.github.com>
2023-10-19 09:13:09 -0400

    Update début fichier projet.py

--- début fichier projet.py
+++ début fichier projet.py
@@ -16,15 +16,14 @@ cylindre=list(LireSTL('cylindre.stl'))
 triangle=list(LireSTL('triangle.stl'))
 mini_planete=list(LireSTL('mini_planete.stl'))
 cube=list(LireSTL('cube.stl'))
-#planete_principal
 
+# Planete principal
 central=homothetie(planete, 2/3)
 
 # Anneaux
 anneau_p = rep_circulaire(rotation(cylindre, np.pi/2,[1,0,0]), 150, np.array([135, 0, 0]), 7) # Répétition circulaire
 anneau_s1 = rep_circulaire(mini_planete, 120, [120, 0, 10], 1/50)
 anneau_s2 = rep_circulaire(mini_planete, 120, [120, 0, -10], 1/50)
-
 
 # Drapeau
     # dimensions
@@ -48,11 +47,9 @@ drapeau = translation(drapeau, np.array([50, -2, -4])) # translation
 minion = centrer(homothetie(minion, 1.25))
 minion_drapeau= fusion([drapeau, minion])
 
-
 # Planète colonisée
 planete = translation(planete, np.array([0, 0, -(max(planete[1][:,2])-min(planete[1][:,2]))/2])) # translation vers z négatifs de la longueur du rayon
 planete_colonisee = fusion([minion_drapeau, planete]) # fusion des deux objets et création du fichier "planete_colonisee.stl"
-
 
 # Satellite
 rectangles = repetition_rectiligne(affinite_vectorielle(cube, 0.5, 1.5, 0.2), 4, 1)
@@ -69,14 +66,14 @@ centre = centrer(affinite_vectorielle(cube, 2, 2.5, 2)) # Centre satellite
 centre = translation(centre, np.array([3.5, 0, -0.8]))
 
 satellite = homothetie(fusion([aile_1, aile_2, centre]), 10)
-satellite = rotation(satellite,np.pi/4,[1,0,0])
-satellite = translation(satellite, np.array([-300, 300, 0]))
+satellite = rotation(satellite,np.pi/8,[0,1,0])
+satellites = rep_circulaire(satellite, 3, np.array([400, 300, 0]), 1)
 
-#Galaxy 
-galaxy=rep_perso(6, planete_colonisee, planete, 2/3) #Répétition selon la suite de fibonacci
-spirale=spirale_perso(mini_planete, 6, 30)
+# Galaxy 
+galaxy = rep_perso(6, planete_colonisee, planete, 2/3) #Répétition selon la suite de fibonacci
+spirale = spirale_perso(mini_planete, 6, 30)
 
-# fusion et export du fichier STL
-scene = fusion([central,anneau_p, anneau_s1, anneau_s2, galaxy, satellite,spirale])
+# Fusion et export du fichier STL
+scene = fusion([central, anneau_p, anneau_s1, anneau_s2, galaxy, satellites, spirale])
 f,v,n = scene[0], scene[1], scene[2]
-EcrireSTLASCII('scene_final_XX.stl', f, v, n)
+EcrireSTLASCII('Scene_25.stl', f, v, n)
